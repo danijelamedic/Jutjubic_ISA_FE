5fd6298ef261d150b89719b6a8d0779f0902a322
Danijela Medic <medic.danijeladaca@gmail.com>
2026-01-16 02:06:51 +0100

    fixed: login home-page issue fixed

--- src/app/core/interceptors/auth.interceptor.ts
+++ src/app/core/interceptors/auth.interceptor.ts
@@ -3,18 +3,19 @@ import { HttpInterceptorFn } from '@angular/common/http';
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('access_token');
 
-  const isAuthEndpoint =
-    req.url.includes('/api/auth/login') ||
-    req.url.includes('/api/auth/register') ||
-    req.url.includes('/api/auth/activate');
+  const isPublicEndpoint = req.url.includes('/api/public');
+  const isAuthEndpoint = req.url.includes('/api/auth');
 
-  if (token && !isAuthEndpoint) {
-    req = req.clone({
-      setHeaders: {
-        Authorization: `Bearer ${token}`
-      }
-    });
+  // Ne šalji Authorization header na public/auth rute
+  if (!token || isPublicEndpoint || isAuthEndpoint) {
+    return next(req);
   }
+
+  req = req.clone({
+    setHeaders: {
+      Authorization: `Bearer ${token}`
+    }
+  });
 
   return next(req);
 };
